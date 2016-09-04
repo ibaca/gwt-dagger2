3216cdb86d9b1a5746a0f2f5ae507652eb7ba4d2
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2016-09-04 19:06:13 +0200

    Apply code splitting using dagger Producers

--- src/main/java/coffee/client/CoffeeApp.java
+++ src/main/java/coffee/client/CoffeeApp.java
@@ -17,18 +17,28 @@
  */
 package coffee.client;
 
+import static com.google.common.util.concurrent.Futures.addCallback;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.SettableFuture;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 import dagger.Binds;
-import dagger.Component;
 import dagger.Module;
 import dagger.Provides;
 import dagger.Subcomponent;
-import java.util.function.Consumer;
-import javax.inject.Inject;
+import dagger.producers.ProducerModule;
+import dagger.producers.Produces;
+import dagger.producers.Production;
+import dagger.producers.ProductionComponent;
+import java.util.concurrent.Executor;
+import javax.annotation.Nullable;
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 public class CoffeeApp implements EntryPoint {
@@ -38,27 +48,27 @@ public class CoffeeApp implements EntryPoint {
         Bootstrapper bootstrapper = DaggerCoffeeApp_Bootstrapper.builder().build();
 
         Button drip = new Button("Drip Coffee");
-        drip.addClickHandler(e -> bootstrapper.dripCoffeeProxy().get(o -> o.maker().brew()));
+        drip.addClickHandler(e -> addCallback(bootstrapper.dripCoffeeMaker(), new FutureCallback<CoffeeMaker>() {
+            public void onSuccess(@Nullable CoffeeMaker result) { result.brew(); }
+            public void onFailure(Throwable t) { GWT.log("error producing " + CoffeeMaker.class.getName(), t); }
+        }));
         RootPanel.get().add(drip);
     }
 
-    @Singleton @Component interface Bootstrapper {
-        DripCoffeeComponent dripCoffeeComponent(DripCoffeeModule dripCoffeeModule);
-        DripCoffeeProxy dripCoffeeProxy();
+    @Singleton @ProductionComponent(modules = BootstrapModule.class) interface Bootstrapper {
+        DripCoffeeComponent dripCoffeeComponent();
+        @Named("drip") ListenableFuture<CoffeeMaker> dripCoffeeMaker();
+    }
 
-        class DripCoffeeProxy {
-            @Inject Bootstrapper bootstrapper;
-            @Inject DripCoffeeProxy() {}
-            void get(Consumer<DripCoffeeComponent> fn) {
-                GWT.runAsync(new RunAsyncCallback() {
-                    @Override public void onFailure(Throwable reason) {
-                        GWT.log("error loading " + DripCoffeeComponent.class.getName(), reason);
-                    }
-                    @Override public void onSuccess() {
-                        fn.accept(bootstrapper.dripCoffeeComponent(new DripCoffeeModule()));
-                    }
-                });
-            }
+    @ProducerModule static class BootstrapModule {
+        @Provides @Production Executor executor() { return MoreExecutors.directExecutor(); }
+        @Produces @Named("drip") ListenableFuture<CoffeeMaker> produceDripCoffeeMaker(Bootstrapper bootstrapper) {
+            SettableFuture<CoffeeMaker> future = SettableFuture.create();
+            GWT.runAsync(new RunAsyncCallback() {
+                @Override public void onFailure(Throwable reason) { future.setException(reason); }
+                @Override public void onSuccess() { future.set(bootstrapper.dripCoffeeComponent().maker()); }
+            });
+            return future;
         }
     }
 
