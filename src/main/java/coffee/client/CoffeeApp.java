2ca566a6cfcc782b96b80c833694aea357839eeb
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2018-02-20 22:22:19 +0100

    Code-split using RxJava instead of Producers

--- src/main/java/coffee/client/CoffeeApp.java
+++ src/main/java/coffee/client/CoffeeApp.java
@@ -1,68 +1,55 @@
 package coffee.client;
 
-import static com.google.common.util.concurrent.Futures.addCallback;
-
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
-import com.google.common.util.concurrent.SettableFuture;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.RunAsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 import dagger.Binds;
+import dagger.Component;
 import dagger.Module;
 import dagger.Provides;
 import dagger.Subcomponent;
-import dagger.producers.ProducerModule;
-import dagger.producers.Produces;
-import dagger.producers.Production;
-import dagger.producers.ProductionComponent;
-import java.util.concurrent.Executor;
-import javax.annotation.Nullable;
 import javax.inject.Named;
+import javax.inject.Singleton;
+import rx.Single;
 
 public class CoffeeApp implements EntryPoint {
 
     @Override public void onModuleLoad() {
         Util.log("GWT+Dagger2 v" + System.getProperty("project.version") + " starting...");
-        Bootstrapper bootstrapper = DaggerCoffeeApp_Bootstrapper.builder().build();
+        Bootstrapper bootstrapper = DaggerCoffeeApp_Bootstrapper.create();
 
         Button drip = new Button("Drip Coffee");
-        drip.addClickHandler(e -> addCallback(bootstrapper.dripCoffeeMaker(), new FutureCallback<CoffeeMaker>() {
-            public void onSuccess(@Nullable CoffeeMaker result) { result.brew(); }
-            public void onFailure(Throwable t) { GWT.log("error producing " + CoffeeMaker.class.getName(), t); }
-        }));
+        drip.addClickHandler(e -> bootstrapper.dripCoffeeMaker().subscribe(CoffeeMaker::brew));
         RootPanel.get().add(drip);
     }
 
-    @ProductionComponent(modules = BootstrapModule.class) interface Bootstrapper {
-        DripCoffeeComponent dripCoffeeComponent();
-        @Named("drip") ListenableFuture<CoffeeMaker> dripCoffeeMaker();
+    @Singleton @Component(modules = BootstrapModule.class) interface Bootstrapper {
+        @Named("drip") Single<CoffeeMaker> dripCoffeeMaker();
     }
 
-    @ProducerModule static class BootstrapModule {
-        @Provides @Production Executor executor() { return MoreExecutors.directExecutor(); }
-        @Produces @Named("drip") ListenableFuture<CoffeeMaker> produceDripCoffeeMaker(Bootstrapper bootstrapper) {
-            SettableFuture<CoffeeMaker> future = SettableFuture.create();
-            GWT.runAsync(new RunAsyncCallback() {
-                @Override public void onFailure(Throwable reason) { future.setException(reason); }
-                @Override public void onSuccess() { future.set(bootstrapper.dripCoffeeComponent().maker()); }
-            });
-            return future;
+    @Module(subcomponents = DripCoffeeComponent.class) static class BootstrapModule {
+        @Provides @Singleton Single<DripCoffeeComponent> dripCoffeeComponent(DripCoffeeComponent.Builder b) {
+            return Single.<DripCoffeeComponent>create(s -> GWT.runAsync(new RunAsyncCallback() {
+                @Override public void onFailure(Throwable reason) { s.onError(reason); }
+                @Override public void onSuccess() { s.onSuccess(b.build()); }
+            })).cache();
+        }
+        @Provides @Singleton @Named("drip") Single<CoffeeMaker> produceDripCoffeeMaker(Single<DripCoffeeComponent> c) {
+            return c.map(DripCoffeeComponent::maker);
         }
     }
 
     @Subcomponent(modules = DripCoffeeModule.class) interface DripCoffeeComponent {
         CoffeeMaker maker();
+        @Subcomponent.Builder interface Builder {
+            DripCoffeeComponent build();
+        }
     }
 
-    @Module(includes = PumpModule.class) static class DripCoffeeModule {
-        @Provides Heater provideHeater() { return new ElectricHeater(); }
-    }
-
-    @Module abstract static class PumpModule {
-        @Binds abstract Pump providePump(Thermosiphon pump);
+    @Module static abstract class DripCoffeeModule {
+        @Provides static Heater heater() { return new ElectricHeater(); }
+        @Binds abstract Pump pump(Thermosiphon pump);
     }
 }
